39e80bc94a742b3ed30f853d11d58ce93a17f128
Brendan-Reid1991 <brendan.reid1991@gmail.com>
2023-07-19 11:01:04 +0100

    Added circuit functions to init

--- src/dotg/circuits/__init__.py
+++ src/dotg/circuits/__init__.py
@@ -1,1 +1,3 @@
 """Top level module for generating circuits of quantum codes, without noise."""
+from dotg.circuits._quantum_circuits import (
+    rotated_surface_code, unrotated_surface_code, color_code)
