36e3c4bc144ce4860cf24b0c7c120ce7bcb4b0fb
Brendan-Reid1991 <brendan.reid1991@gmail.com>
2023-08-24 13:23:35 +0100

    LDPC decoder shell classes

---
+++ src/dotg/decoders/_belief_propagation.py
@@ -0,0 +1,9 @@
+"""This module provides access to the Belief-Propagation decoder from the ldpc package: 
+https://github.com/quantumgizmos/ldpc"""
+
+
+class BeliefPropagation:
+    """"""
+
+    def __init__(self) -> None:
+        pass
---
+++ src/dotg/decoders/_bposd.py
@@ -0,0 +1,9 @@
+"""This module provides access to the BPOSD decoder from the ldpc package: 
+https://github.com/quantumgizmos/ldpc"""
+
+
+class BPOSD:
+    """ """
+
+    def __init__(self) -> None:
+        pass
