14745103d87ebd2399d382b0d835155b0226dff1
Brendan-Reid1991 <brendan.reid1991@gmail.com>
2023-08-24 13:39:51 +0100

    Added CircuitUnderstander to init

--- src/dotg/utilities/__init__.py
+++ src/dotg/utilities/__init__.py
@@ -1,8 +1,14 @@
 """Top level module for utilities"""
-from dotg.utilities._check_matrices import CircuitUnderstander
+from dotg.utilities._parity_matrices import CircuitUnderstander
 from dotg.utilities._stim_decorators import StimDecorators
-from dotg.utilities._stim_gates import (MeasureAndReset, MeasurementGates,
-                                        OneQubitGates, ResetGates,
-                                        TwoQubitGates)
-from dotg.utilities._stim_noise_channels import (OneQubitNoiseChannels,
-                                                 TwoQubitNoiseChannels)
+from dotg.utilities._stim_gates import (
+    MeasureAndReset,
+    MeasurementGates,
+    OneQubitGates,
+    ResetGates,
+    TwoQubitGates,
+)
+from dotg.utilities._stim_noise_channels import (
+    OneQubitNoiseChannels,
+    TwoQubitNoiseChannels,
+)
