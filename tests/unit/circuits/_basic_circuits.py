7b16805ecb2d85b244b9789fa83166323ef7553a
Brendan-Reid1991 <brendan.reid1991@gmail.com>
2023-08-25 23:58:32 +0100

    made HypergraphLike circuit an actual hypergraph

--- tests/unit/circuits/_basic_circuits.py
+++ tests/unit/circuits/_basic_circuits.py
@@ -15,7 +15,7 @@ class BasicCircuits:
         )
 
     class HypergraphLike:
-        NOISELESS_CIRCUIT = color_code(distance=3, rounds=2)
+        NOISELESS_CIRCUIT = color_code(distance=5, rounds=3)
 
         _noise_model = DepolarizingNoise(physical_error=1e-2)
         NOISY_CIRCUIT = _noise_model.permute_circuit(NOISELESS_CIRCUIT)
@@ -24,12 +24,3 @@ class BasicCircuits:
         MEASUREMENT_NOISE_ONLY_CIRCUIT = _measurement_noise_only.permute_circuit(
             NOISELESS_CIRCUIT
         )
-
-    # self.NOISELESS_CIRCUIT = rotated_surface_code(distance=2, rounds=1)
-    # _noise_model = DepolarizingNoise(physical_error=1e-2)
-    # NOISY_CIRCUIT = _noise_model.permute_circuit(NOISELESS_CIRCUIT)
-
-    # measurement_noise_only = NoiseModel(measurement_noise=1e-2)
-    # MEASUREMENT_NOISE_ONLY_CIRCUIT = measurement_noise_only.permute_circuit(
-    #     NOISELESS_CIRCUIT
-    # )
