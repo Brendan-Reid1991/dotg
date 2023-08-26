14b60476931f6b540bb9b6663949549efd23a6d5
Brendan-Reid1991 <brendan.reid1991@gmail.com>
2023-08-26 10:55:11 +0100

    Black

--- src/dotg/utilities/stim_assets/__init__.py
+++ src/dotg/utilities/stim_assets/__init__.py
@@ -1,12 +1,7 @@
 from dotg.utilities.stim_assets._stim_decorators import StimDecorators
-from dotg.utilities.stim_assets._stim_gates import (
-    MeasureAndReset,
-    MeasurementGates,
-    OneQubitGates,
-    ResetGates,
-    TwoQubitGates,
-)
+from dotg.utilities.stim_assets._stim_gates import (MeasureAndReset,
+                                                    MeasurementGates,
+                                                    OneQubitGates, ResetGates,
+                                                    TwoQubitGates)
 from dotg.utilities.stim_assets._stim_noise_channels import (
-    OneQubitNoiseChannels,
-    TwoQubitNoiseChannels,
-)
+    OneQubitNoiseChannels, TwoQubitNoiseChannels)
--- tests/unit/decoders/_basic_decoder_tests.py
+++ tests/unit/decoders/_basic_decoder_tests.py
@@ -1,4 +1,5 @@
 from typing import Type
+
 import pytest
 
 from dotg.decoders._decoder_base_class import Decoder
@@ -9,8 +10,16 @@ from tests.unit.circuits import BasicCircuits
 class BasicDecoderTests:
     DECODER_CLASS: Type[Decoder]
 
-    def test_raises_NoNoiseError_for_no_noise(self):
-        with pytest.raises(NoNoiseInCircuitError, match=NoNoiseInCircuitError().args[0]):
-            self.DECODER_CLASS(BasicCircuits.GraphLike.NOISELESS_CIRCUIT)
-        with pytest.raises(NoNoiseInCircuitError, match=NoNoiseInCircuitError().args[0]):
-            self.DECODER_CLASS(BasicCircuits.HypergraphLike.NOISELESS_CIRCUIT)
+    def test_raises_NoNoiseError_for_no_noise(self, *args, **kwargs):
+        with pytest.raises(
+            NoNoiseInCircuitError, match=NoNoiseInCircuitError().args[0]
+        ):
+            self.DECODER_CLASS(
+                BasicCircuits.GraphLike.NOISELESS_CIRCUIT, *args, **kwargs
+            )
+        with pytest.raises(
+            NoNoiseInCircuitError, match=NoNoiseInCircuitError().args[0]
+        ):
+            self.DECODER_CLASS(
+                BasicCircuits.HypergraphLike.NOISELESS_CIRCUIT, *args, **kwargs
+            )
