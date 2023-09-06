14f6cb4d59f3c74e628cf0b7997f792f6829d5f4
Brendan-Reid1991 <brendan.reid1991@gmail.com>
2023-09-06 21:19:10 +0100

    Split basic decoder tests

---
+++ tests/unit/decoders/_basic_tests/_basic_decoder_tests.py
@@ -0,0 +1,22 @@
+from typing import Type
+
+import pytest
+
+from dotg.decoders._decoder_base_class import Decoder
+
+from dotg.utilities._syndrome_sampler import NoNoiseInCircuitError
+from tests.unit.circuits import BasicCircuits
+
+
+class BasicDecoderTests:
+    DECODER_CLASS: Type[Decoder]
+
+    def test_raises_NoNoiseError_for_no_noise(self, *args, **kwargs):
+        with pytest.raises(NoNoiseInCircuitError, match=NoNoiseInCircuitError().args[0]):
+            self.DECODER_CLASS(
+                BasicCircuits.GraphLike.NOISELESS_CIRCUIT, *args, **kwargs
+            )
+        with pytest.raises(NoNoiseInCircuitError, match=NoNoiseInCircuitError().args[0]):
+            self.DECODER_CLASS(
+                BasicCircuits.HypergraphLike.NOISELESS_CIRCUIT, *args, **kwargs
+            )
