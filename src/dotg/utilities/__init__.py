4aedec80e577938cc9792f72a18e523dba2be6de
Brendan-Reid1991 <brendan.reid1991@gmail.com>
2023-09-07 09:07:19 +0100

    Add circuit layers function

--- src/dotg/utilities/__init__.py
+++ src/dotg/utilities/__init__.py
@@ -1,3 +1,4 @@
 """Top level module for utilities"""
 from dotg.utilities._check_matrices import CircuitUnderstander
+from dotg.utilities._circuit_layers import get_circuit_layers
 from dotg.utilities._syndrome_sampler import Sampler
---
+++ src/dotg/utilities/_circuit_layers.py
@@ -0,0 +1,39 @@
+"""This module provides a function to divide a stim circuit into timelike layers."""
+from typing import List
+
+import stim
+
+from dotg.utilities.stim_assets import StimDecorators
+
+
+def get_circuit_layers(circuit: stim.Circuit) -> List[stim.Circuit]:
+    """Divide a stim circuit into a list of subcircuits, each element representing one
+    timestep of the circuit.
+
+    Parameters
+    ----------
+    circuit : stim.Circuit
+        Stim circuit to divide.
+
+    Returns
+    -------
+    List[stim.Circuit]
+        List of stim circuits, where each element represents one timestep of the circuit.
+    """
+    layers = [
+        idx + 1
+        for idx, instr in enumerate(circuit)
+        if instr.name == StimDecorators.TICK.value
+    ]
+    circuit_by_layers = [
+        (list(circuit) + [""])[slice(ix, iy)]
+        for ix, iy in zip([0] + layers, layers + [-1])
+    ]
+    sub_circuits = []
+    for layer in circuit_by_layers:
+        new_circ = stim.Circuit()
+        for instr in layer:
+            new_circ.append(name=instr)
+        sub_circuits.append(new_circ)
+
+    return sub_circuits
