f5b11d2e56a89f2f465269766bcc3b9e7668525d
Brendan-Reid1991 <brendan.reid1991@gmail.com>
2023-09-07 09:16:57 +0100

    Added basic tests

---
+++ tests/unit/utilities/test_circuit_layers.py
@@ -0,0 +1,27 @@
+import pytest
+import stim
+from dotg.utilities import get_circuit_layers
+
+
+class TestGetCircuitLayers:
+    @pytest.fixture(scope="class")
+    def simple_circuit(self):
+        return stim.Circuit(
+            """
+            R 0 1 2
+            TICK
+            M 0 1 2
+            """
+        )
+
+    def test_basic_functionality(self, simple_circuit):
+        subcircuits = get_circuit_layers(simple_circuit)
+        assert (
+            len(subcircuits) == 2
+        ), "Fixture `simple_circuit` should result in two subcircuits."
+        assert all(
+            isinstance(x, stim.Circuit) for x in subcircuits
+        ), "Each element should be of type stim.Circuit."
+        assert isinstance(
+            subcircuits, list
+        ), "Output from get_circuit_layers is expected to be a list."
