04144619829ba661f5e37fee958b2234f415c8e8
Brendan-Reid1991 <brendan.reid1991@gmail.com>
2023-09-10 15:39:33 +0100

    removed

--- experiments/_noisy_circuits.py
+++
@@ -1,9 +0,0 @@
-from dotg.circuits import rotated_surface_code
-from dotg.noise import DepolarizingNoise
-
-if __name__ == "__main__":
-    noise_model = DepolarizingNoise(physical_error=1e-3)
-    circuit = rotated_surface_code(distance=2)
-    print(circuit)
-    circuit = noise_model.permute_circuit(circuit)
-    print(circuit)
