96554bd7cc427a339c14d3e1ac3890ee26e85ba2
Brendan-Reid1991 <brendan.reid1991@gmail.com>
2023-09-13 13:04:58 +0100

    Update init

--- src/dotg/circuits/quantum_memory/__init__.py
+++ src/dotg/circuits/quantum_memory/__init__.py
@@ -1,6 +1,3 @@
 """Top level module for generating circuits of quantum codes, without noise."""
-from dotg.circuits.quantum_memory._quantum_circuits import (
-    color_code,
-    rotated_surface_code,
-    unrotated_surface_code,
-)
+from dotg.circuits.quantum_memory._surface_codes import SurfaceCode
+from dotg.circuits.quantum_memory._color_codes import ColorCode
