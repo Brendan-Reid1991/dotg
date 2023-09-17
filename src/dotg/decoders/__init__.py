2afce0741338d9cc576725f742b19c9356ee6178
Brendan-Reid1991 <brendan.reid1991@gmail.com>
2023-09-17 19:21:58 +0100

    Added decoder options to top level init

--- src/dotg/decoders/__init__.py
+++ src/dotg/decoders/__init__.py
@@ -1,4 +1,9 @@
 """Top level init for dotg.decoders"""
 from dotg.decoders._belief_propagation import BeliefPropagation
+from dotg.decoders._belief_propagation_base_class import (
+    LDPCDecoderOptions,
+    MessageUpdates,
+    OSDMethods,
+)
 from dotg.decoders._bposd import BPOSD
 from dotg.decoders._pymatching import MinimumWeightPerfectMatching
