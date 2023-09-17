77b34a0eed6fd4dd3b0a848b9e9a188339dffab5
Brendan-Reid1991 <brendan.reid1991@gmail.com>
2023-09-17 19:44:33 +0100

    Tests for BPOSD

--- tests/unit/decoders/test_bposd.py
+++ tests/unit/decoders/test_bposd.py
@@ -13,6 +13,41 @@ from tests.unit.decoders._basic_tests._basic_bp_bposd_tests import (
 from tests.unit.decoders._basic_tests._basic_decoder_tests import BasicDecoderTests
 
 
-@pytest.mark.xfail(reason="Haven't gotten around to this one yet.")
 class TestBPOSD(BasicBeliefPropagationDecoderTests, BasicDecoderTests):
     DECODER_CLASS = BPOSD
+
+    def test_error_raised_if_no_osd_method_passed(self):
+        with pytest.raises(
+            ValueError, match="You must provide an OSD method to use BPOSD."
+        ):
+            BPOSD(
+                circuit=BasicCircuits.GraphLike.NOISY_CIRCUIT,
+                decoder_options=LDPCDecoderOptions(max_iterations=20),
+            )
+
+    @pytest.fixture(scope="class")
+    def options(self):
+        return LDPCDecoderOptions(
+            max_iterations=2, osd_method=OSDMethods.EXHAUSTIVE, osd_order=2
+        )
+
+    @pytest.fixture(scope="class")
+    def decoder_graph(self, options):
+        return self.DECODER_CLASS(
+            circuit=BasicCircuits.GraphLike.NOISY_CIRCUIT,
+            decoder_options=options,
+        )
+
+    @pytest.fixture(scope="class")
+    def decoder_hypergraph(self, options):
+        return self.DECODER_CLASS(
+            circuit=BasicCircuits.HypergraphLike.NOISY_CIRCUIT,
+            decoder_options=options,
+        )
+
+    def test_raises_NoNoiseError_for_no_noise(self, options):
+        return super().test_raises_NoNoiseError_for_no_noise(options)
+
+    # TODO Rewrite this test!!
+    def test_logical_error(self, decoder_hypergraph):
+        decoder_hypergraph.logical_error(10)
