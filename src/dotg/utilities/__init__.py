c81b43933ea2929cea03335be9d6d0f097b57ab4
Brendan-Reid1991 <brendan.reid1991@gmail.com>
2023-09-19 17:49:00 +0100

    Added thresholdheuristic to init

--- src/dotg/utilities/__init__.py
+++ src/dotg/utilities/__init__.py
@@ -2,3 +2,4 @@
 from dotg.utilities._check_matrices import CircuitUnderstander
 from dotg.utilities._circuit_layers import get_circuit_layers
 from dotg.utilities._syndrome_sampler import Sampler
+from dotg.utilities._threshold import ThresholdHeuristic
