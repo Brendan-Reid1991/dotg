28d9d5e5a62ace7f08e587f6d3bbabfd98087f99
Brendan-Reid1991 <brendan.reid1991@gmail.com>
2023-10-17 10:02:18 +0200

    robust parallelisation will take some work

--- src/dotg/decoders/_manager.py
+++ src/dotg/decoders/_manager.py
@@ -2,8 +2,15 @@ from __future__ import annotations
 from typing import Optional
 import multiprocessing as mp
 import numpy as np
+from dotg.decoders import BPOSD, LDPCDecoderOptions, OSDMethods
 from dotg.decoders._decoder_base_class import Decoder
 from dotg.decoders._belief_propagation_base_class import LDPCBeliefPropagationDecoder
+
+from dotg.circuits import SurfaceCode
+from dotg.noise import DepolarizingNoise
+
+code = SurfaceCode.Rotated(distance=5)
+circuit = DepolarizingNoise(physical_error=1e-3).permute_circuit(code.memory)
 
 
 class DecoderManager:
@@ -11,15 +18,53 @@ class DecoderManager:
         self.decoder = decoder
         self.cores = cores or mp.cpu_count()
 
-    def _get_logical_error(self, num_shots: int | float):
-        return self.decoder.logical_error(num_shots=num_shots)
+    def _parallelise_cython(self, num_shots: int | float):
+        decoder = BPOSD(
+            circuit=circuit,
+            decoder_options=LDPCDecoderOptions(
+                max_iterations=5, osd_method=OSDMethods.EXHAUSTIVE
+            ),
+        )
+        return decoder.logical_error(num_shots=num_shots)
 
     def run(self, num_shots: int | float):
         shots_per_core = num_shots // self.cores
 
         if isinstance(self.decoder, LDPCBeliefPropagationDecoder):
-            raise ValueError("These must be parallelised manually.")
+            with mp.Pool(self.cores) as pool:
+                results = pool.map(
+                    self._parallelise_cython,
+                    [shots_per_core] * self.cores,
+                )
+            return np.mean(results), np.std(results)
 
-        with mp.Pool(processes=self.cores) as pool:
-            results = pool.map(self._get_logical_error, [shots_per_core] * self.cores)
+        with mp.Pool(self.cores) as pool:
+            results = pool.map(self.decoder.logical_error, [shots_per_core] * self.cores)
         return np.mean(results), np.std(results)
+
+
+if __name__ == "__main__":
+    from dotg.decoders import (
+        BPOSD,
+        LDPCDecoderOptions,
+        OSDMethods,
+        MinimumWeightPerfectMatching,
+    )
+    from dotg.decoders._manager import DecoderManager
+    from dotg.circuits import SurfaceCode
+    from dotg.noise import DepolarizingNoise
+
+    code = SurfaceCode.Rotated(distance=5)
+    circuit = DepolarizingNoise(physical_error=1e-3).permute_circuit(code.memory)
+
+    # decoder = BPOSD(
+    #     circuit=circuit,
+    #     decoder_options=LDPCDecoderOptions(
+    #         max_iterations=5, osd_method=OSDMethods.EXHAUSTIVE
+    #     ),
+    # )
+    decoder = MinimumWeightPerfectMatching(circuit=circuit)
+
+    manager = DecoderManager(decoder=decoder)
+
+    print(manager.run(num_shots=int(1e4)))
