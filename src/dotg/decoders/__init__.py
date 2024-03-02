4bbd5b5dc7450119ec443381773847ef9bb77514
Brendan-Reid1991 <brendan.reid1991@gmail.com>
2024-03-02 17:53:57 +0000

    Moved DecoderManager into init

--- src/dotg/decoders/__init__.py
+++ src/dotg/decoders/__init__.py
@@ -2,10 +2,10 @@
 
 from dotg.decoders._belief_propagation import BeliefPropagation
 from dotg.decoders._decoder_base_classes import (
-    Decoder,
     LDPCDecoderOptions,
     MessageUpdates,
     OSDMethods,
 )
 from dotg.decoders._bposd import BPOSD
 from dotg.decoders._pymatching import MinimumWeightPerfectMatching
+from dotg.decoders._manager import DecoderManager
