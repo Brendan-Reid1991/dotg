cb1fcc224a451865be9672253bfa82d74e4a36d4
Brendan-Reid1991 <brendan.reid1991@gmail.com>
2024-10-18 23:48:44 +0100

    Added utilities init

---
+++ src/builder/utilities/__init__.py
@@ -0,0 +1,5 @@
+"""The utilities module provides helpful objects and functions
+for building quantum circuits."""
+
+from builder.utilities._pauliproduct import PauliProduct
+from builder.utilities._qubit_coordinate import QubitCoordinate
