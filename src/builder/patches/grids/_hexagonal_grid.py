a9e7a90105a32fa84a75bc92fb8784aa31a76676
Brendan-Reid1991 <brendan.reid1991@gmail.com>
2024-10-21 22:29:48 +0100

    Refactor: Made hex grid class cleaner

--- src/builder/patches/grids/_hexagonal_grid.py
+++ src/builder/patches/grids/_hexagonal_grid.py
@@ -9,6 +9,40 @@ from builder.utilities import QubitCoordinate
 
 
 class HexagonalGrid:
+    """A hexagonal grid structure, used to define the triangular
+    color code.
+
+    Parameters
+    ----------
+    x_lim: int
+        The max x-coordinate on the grid.
+    y_lim: int
+        The max y-coordinate on the grid.
+
+    Attributes
+    ----------
+    data_qubits: list[QubitCoordinate]
+        A list of data qubits defined on the grid.
+    z_stabilizers: list[QubitCoordinate]
+        A list of Z stabilizers defined on the grid.
+    x_stabilizers: list[QubitCoordinate]
+        A list of X stabilizers defined on the grid.
+    coordinate_mapping: dict[QubitCoordinate, int]
+        A dictionary that maps a qubit coordinate to its index.
+    schedules: dict[str, list[SquareGrid.Displacer]]
+        Default schedules for X and Z syndrome extraction circuits,
+        accessed by schedules['x'] and schedules['z'] respectively.
+        Values of these keys are the time-step indexed interaction
+        order, i.e.
+        >>> schedules['x'] = [SquareGrid.Displacer.TOP_LEFT,
+                            SquareGrid.Displacer.TOP_RIGHT,
+                            SquareGrid.Displacer.BOTTOM_LEFT,
+                            SquareGrid.Displacer.BOTTOM_RIGHT]
+        The elements of this list can be added to an X stabilizer
+        qubit to determine which data qubit it should interact with
+        in that timestep.
+    """
+
     class Displacer(Enum):
         NE: tuple[float, float] = (0.5, 1)
         E: tuple[float, float] = (1, 0)
@@ -17,52 +51,120 @@ class HexagonalGrid:
         W: tuple[float, float] = (-1, 0)
         NW: tuple[float, float] = (-0.5, 1)
 
-    def __init__(self, code_distance: int, anchor: QubitCoordinate) -> None:
-        self._N = code_distance + code_distance // 2
-        self.anchor = QubitCoordinate(*anchor)
-        qubits = []
-        for y in np.arange(0, self._N):
-            for x in np.arange(y / 2, self._N - y / 2):
-                qubits.append(QubitCoordinate(x, y))
+    def __init__(self, x_lim: int, y_lim: int) -> None:
+        self._x_lim = x_lim
+        self._y_lim = y_lim
 
-        self.red_qubits = [
-            q for q in qubits if q.y == 0 and q.x in np.arange(1, self._N, 3)
-        ]
-        self.blue_qubits = [
-            q for q in qubits if q.y == 1 and q.x in np.arange(2.5, self._N, 3)
-        ]
-        self.green_qubits = [
-            q for q in qubits if q.y == 2 and q.x in np.arange(1, self._N, 3)
-        ]
-        for color in [self.red_qubits, self.blue_qubits, self.green_qubits]:
-            additions = []
-            for q in color:
-                while True:
-                    q = q + (1.5, 3)
-                    if q in qubits:
-                        additions.append(q)
-                        continue
-                    break
-            color += additions
-        self.data_qubits = [
-            q
-            for q in qubits
-            if q not in self.red_qubits + self.blue_qubits + self.green_qubits
+        (
+            self.data_qubits,
+            self.red_qubits,
+            self.blue_qubits,
+            self.green_qubits,
+            self.coordinate_mapping,
+        ) = self._initialize()
+
+    def _get_data_qubits(self) -> list[QubitCoordinate]:
+        """Get the data qubits on the grid.
+
+        Returns
+        -------
+        list[QubitCoordinate]
+        """
+        return [
+            QubitCoordinate(x, y)
+            for x in range(0, self._x_lim + 1)
+            for y in range(0, self._y_lim + 1)
         ]
 
-        self.red_qubits = sorted(self.red_qubits, key=lambda x: (x[1]))
-        self.green_qubits = sorted(self.green_qubits, key=lambda x: (x[1]))
-        self.blue_qubits = sorted(self.blue_qubits, key=lambda x: (x[1]))
+    def _get_red_stabilizer_qubits(self) -> list[QubitCoordinate]:
+        """Get the red stabilizer qubits.
 
-        for idx, q in enumerate(
-            self.data_qubits + self.red_qubits + self.blue_qubits + self.green_qubits
-        ):
-            q.idx = idx
+        Returns
+        -------
+        list[QubitCoordinate]
+        """
+        return sorted(
+            self._add_bulk_stabilizers(
+                [QubitCoordinate(x, 0) for x in np.arange(1, self._x_lim + 1, 3)]
+            ),
+            key=lambda x: x[1],
+        )
+
+    def _get_blue_stabilizer_qubits(self) -> list[QubitCoordinate]:
+        """Get the red stabilizer qubits.
+
+        Returns
+        -------
+        list[QubitCoordinate]
+        """
+        return sorted(
+            self._add_bulk_stabilizers(
+                [QubitCoordinate(x, 1) for x in np.arange(2.5, self._x_lim + 1, 3)]
+            ),
+            key=lambda x: x[1],
+        )
+
+    def _get_green_stabilizer_qubits(self) -> list[QubitCoordinate]:
+        """Get the green stabilizer qubits.
+
+        Returns
+        -------
+        list[QubitCoordinate]
+        """
+        return sorted(
+            self._add_bulk_stabilizers(
+                [QubitCoordinate(x, 2) for x in np.arange(1, self._x_lim + 1, 3)]
+            ),
+            key=lambda x: x[1],
+        )
+
+    def _add_bulk_stabilizers(
+        self, colored_stabilizers: list[QubitCoordinate]
+    ) -> list[QubitCoordinate]:
+        """Add bulk stabilizers to a list of colored stabilizers,
+        by stepping across the hexagonal lattice in a measured fashion.
+
+        Parameters
+        ----------
+        colored_stabilizers : list[QubitCoordinate]
+            A list of initial colored stabilizers.
+
+        Returns
+        -------
+        list[QubitCoordinate]
+            A list of more stabilizers of the same color.
+        """
+        additions = []
+        for q in colored_stabilizers:
+            while True:
+                q = q + (1.5, 3)
+                if q.x <= self._x_lim and q.y <= self._y_lim:
+                    additions.append(q)
+                    continue
+                break
+        colored_stabilizers += additions
+        return colored_stabilizers
 
     def _get_neighbour(
-        self, stabilizer: QubitCoordinate, displacer: Displacer
+        self, qubit: QubitCoordinate, displacer: Displacer
     ) -> QubitCoordinate | None:
-        neighbour = stabilizer + displacer
+        """Given a qubit coordinate and a displacement, get
+        the neighbouring qubit if it exists.
+
+        Parameters
+        ----------
+        qubit : QubitCoordinate
+            Qubit to check neighbours of.
+        displacer : Displacer
+            A displacement to look for another qubit.
+
+        Returns
+        -------
+        QubitCoordinate | None
+            A qubit coordinate or None, if no qubit exists at
+            that displacement.
+        """
+        neighbour = qubit + displacer
         try:
             return next(q for q in self.data_qubits if q == neighbour)
         except StopIteration:
@@ -71,165 +173,58 @@ class HexagonalGrid:
     def stabilizer_data_qubit_groups(
         self, stabilizer: QubitCoordinate
     ) -> list[QubitCoordinate]:
-        data_qubits = []
-        for displacement in ColorCodePatch.Displacer._value2member_map_:
-            q = self._get_neighbour(stabilizer, displacement)
-            if q:
-                data_qubits.append(q)
-
-        return data_qubits
-
-    def to_stabilizers(
-        self, return_logical: Optional[str] = None
-    ) -> tuple[list[str], list[str], str] | tuple[list[str], list[str]]:
-        xs = []
-        zs = []
-        for stabilizer in self.red_qubits + self.green_qubits + self.blue_qubits:
-            data_qubits = self.stabilizer_data_qubit_groups(stabilizer=stabilizer)
-            for _type in ["X", "Z"]:
-                template = ["_"] * len(self.data_qubits)
-                for dq in data_qubits:
-                    template[dq.idx] = _type
-                if _type == "X":
-                    xs.append("".join(template))
-                else:
-                    zs.append("".join(template))
-        if return_logical:
-            logical = "".join([return_logical] * len(self.data_qubits))
-            return xs, zs, logical
-        return xs, zs
-
-    def draw(
-        self,
-        figure: Optional[matplotlib.figure.Figure] = None,
-        indices: bool = False,
-    ) -> matplotlib.figure.Figure:
-        """Draw the patch on the grid, colouring in the stabilizers and optionally adding the indices.
+        """Given a stabilizer qubit, return a list of
+        the data qubits incident on the stabilizer.
 
         Parameters
         ----------
-        figure: matplotlib.figure.Figure, optional.
-            The matplotlib figure to draw on. If not supplied, one is created.
-        indices : bool, optional
-            Add index labels to the qubits, by default False
+        stabilizer : QubitCoordinate
+            Which stabilizer to consider.
 
         Returns
         -------
-        matplotlib.figure.Figure
-            A matplotlib Figure object.
+        list[QubitCoordinate]
+            List of data qubits incident on the stabilizer.
         """
+        return [
+            q
+            for displacement in HexagonalGrid.Displacer._value2member_map_
+            if (q := self._get_neighbour(stabilizer, displacement))
+        ]
 
-        if figure:
-            fig = figure
-            ax = figure.axes[0]
-        else:
-            plt.style.use("fast")
-            fig, ax = plt.subplots(1, 1, figsize=(15, 12))
+    def _initialize(
+        self,
+    ) -> tuple[
+        list[QubitCoordinate],
+        list[QubitCoordinate],
+        list[QubitCoordinate],
+        list[QubitCoordinate],
+        dict[QubitCoordinate, int],
+    ]:
+        """Initialize the grid.
 
-        def draw_data_qubit(data_qubit: QubitCoordinate) -> matplotlib.patches.Patch:
-            """Draw a circle where this data qubit should be.
-
-            Parameters
-            ----------
-            data_qubit : QubitCoordinate
-                Data qubit coordinate.
-
-            Returns
-            -------
-            matplotlib.patches.Patch
-                Patch object to be added to the figure.
-            """
-            return plt.Circle(
-                xy=data_qubit,
-                radius=0.25,
-                linewidth=1,
-                edgecolor="black",
-                facecolor="white",
-                zorder=3,
-            )
-
-        def draw_polygon(stabilizer: QubitCoordinate) -> matplotlib.patches.Patch:
-            color = (
-                "red"
-                if stabilizer in self.red_qubits
-                else "green"
-                if stabilizer in self.green_qubits
-                else "blue"
-            )
-            return matplotlib.patches.Polygon(
-                xy=self.stabilizer_data_qubit_groups(stabilizer=stabilizer),
-                color=color,
-                alpha=0.4,
-                zorder=2,
-                snap=True,
-                label=stabilizer,
-            )
-
-        def draw_grid(ax: matplotlib.axes.Axes) -> matplotlib.axes.Axes:
-            """Add a grid to the figure, to guide the eye.
-
-            Parameters
-            ----------
-            ax : matplotlib.axes.Axes
-                Figure axis.
-
-            Returns
-            -------
-            matplotlib.axes.Axes
-                The same axis, but now with gridlines.
-            """
-            for i in range(self._N):
-                ax.vlines(
-                    i,
-                    0,
-                    self._N,
-                    colors="black",
-                    linestyles="dashed",
-                    alpha=0.1,
-                    zorder=1,
-                )
-            for j in range(self._N):
-                ax.hlines(
-                    j,
-                    0,
-                    self._N,
-                    colors="black",
-                    linestyles="dashed",
-                    alpha=0.1,
-                    zorder=1,
-                )
-            return ax
-
-        ax = draw_grid(ax=ax)
-        for qubit in (
-            self.data_qubits + self.green_qubits + self.red_qubits + self.blue_qubits
+        Returns
+        -------
+        tuple[ list[QubitCoordinate],
+                list[QubitCoordinate],
+                list[QubitCoordinate],
+                list[QubitCoordinate],
+                dict[QubitCoordinate, int] ]
+            In order:
+            - List of data qubits
+            - List of RED stabilizers
+            - List of BLUE stabilizers
+            - List of GREEN stabilizers
+            - Qubit coordinate to qubit index dictionary.
+        """
+        data_qubits = self._get_data_qubits()
+        red_qubits = self._get_red_stabilizer_qubits()
+        blue_qubits = self._get_blue_stabilizer_qubits()
+        green_qubits = self._get_green_stabilizer_qubits()
+        coordinate_mapping: dict[QubitCoordinate, int] = {}
+        for idx, qubit in enumerate(
+            data_qubits + red_qubits + blue_qubits + green_qubits
         ):
-            if qubit in self.data_qubits:
-                patch = draw_data_qubit(data_qubit=qubit)
-            else:
-                patch = draw_polygon(stabilizer=qubit)
-            ax.add_patch(patch)
-            if indices:
-                polygon: bool = isinstance(patch, matplotlib.patches.Polygon)
-                if polygon:
-                    xy = ast.literal_eval(patch._label)
+            coordinate_mapping[qubit] = idx
 
-                else:
-                    xy = patch.center
-                ax.annotate(
-                    f"{str(qubit.idx)}",
-                    xy=xy,
-                    color="black",
-                    style="oblique",
-                    fontsize=17.5,
-                    ha="center",
-                    va="center",
-                )
-
-        plt.gca().set_aspect("equal")
-
-        ax.tick_params(which="both", width=2)
-        ax.tick_params(which="major", length=7)
-        # ax.tick_params(which="minor", length=4)
-
-        return fig
+        return data_qubits, red_qubits, blue_qubits, green_qubits, coordinate_mapping
