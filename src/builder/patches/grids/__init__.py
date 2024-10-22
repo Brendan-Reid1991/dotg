cdff94f31e9d902248377ac278beebc301f01329
Brendan-Reid1991 <brendan.reid1991@gmail.com>
2024-10-23 00:10:55 +0100

    Added grids docstring

--- src/builder/patches/grids/__init__.py
+++ src/builder/patches/grids/__init__.py
@@ -1,2 +1,5 @@
+"""The grids package defines some qubit grid classes; 
+these control qubit indexing and classification."""
+
 from builder.patches.grids._square_grid import SquareGrid
 from builder.patches.grids._hexagonal_grid import HexagonalGrid
