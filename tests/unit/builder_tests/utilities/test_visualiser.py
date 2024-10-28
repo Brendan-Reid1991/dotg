1e8240fc820f3e308ac1f809177796ced23d31db
Brendan-Reid1991 <brendan.reid1991@gmail.com>
2024-10-28 16:42:36 +0000

    Updates to tests after refactor

--- tests/unit/builder_tests/utilities/test_visualiser.py
+++ tests/unit/builder_tests/utilities/test_visualiser.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 from builder.utilities._qubit_coordinate import QubitCoordinate
-from builder.patches.grids import SquareGrid, HexagonalGrid
+from builder.utilities.grids import SquareGrid, HexagonalGrid
 
 from builder.utilities._visualiser import Visualiser
 
--- tests/unit/dotg_tests/utilities/stim_assets/test_stim_annotations.py
+++ tests/unit/dotg_tests/utilities/stim_assets/test_stim_annotations.py
@@ -2,14 +2,15 @@ from dotg.utilities.stim_assets import StimAnnotations
 
 from .base_test_stim_operations import BaseTestStimOperations
 
-CURRENT_DECORATORS = [
+CURRENT_ANNOTATIONS = [
     "DETECTOR",
     "OBSERVABLE_INCLUDE",
     "QUBIT_COORDS",
     "TICK",
+    "SHIFT_COORDS",
 ]
 
 
 class TestStimDecorators(BaseTestStimOperations):
     ENUM = StimAnnotations
-    CURRENT_MEMBERS = CURRENT_DECORATORS
+    CURRENT_MEMBERS = CURRENT_ANNOTATIONS
