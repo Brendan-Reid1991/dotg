1a4c43aa86b3fa62b994738059f9cf3c674b2753
Brendan-Reid1991 <brendan.reid1991@gmail.com>
2024-10-28 13:45:43 +0000

    Added the QubitGrid base class

---
+++ src/builder/utilities/grids/_grid.py
@@ -0,0 +1,83 @@
+"""A base class for qubit grids."""
+
+from abc import abstractmethod
+from typing import TypeAlias
+from builder.utilities._qubit_coordinate import QubitCoordinate
+
+Displacer: TypeAlias = tuple[float, float]
+
+
+class QubitGrid:
+    """A simple base class for qubit grids."""
+
+    def __init__(self, *args, **kwargs) -> None:
+        self._data_qubits: list[QubitCoordinate] = []
+
+    @property
+    def data_qubits(self) -> list[QubitCoordinate]:
+        """Get a list of the data qubits in the grid.
+
+        Returns
+        -------
+        list[QubitCoordinate]
+            Data qubits in the grid.
+
+        Raises
+        ------
+        ValueError
+            If the attribute has not been set correctly.
+        """
+        if len(self._data_qubits) == 0:
+            raise ValueError(
+                f"""Grid class {self.__class__.__name__} must allocate an attribute
+                data_qubits."""
+            )
+        return self._data_qubits
+
+    @data_qubits.setter
+    def data_qubits(self, updated_list_of_data_qubits: list[QubitCoordinate]) -> None:
+        self._data_qubits = updated_list_of_data_qubits
+
+    def _get_neighbour(
+        self, qubit: QubitCoordinate, displacer: Displacer
+    ) -> QubitCoordinate | None:
+        """Given a qubit coordinate and a displacement, get
+        the neighbouring qubit if it exists.
+
+        Parameters
+        ----------
+        qubit : QubitCoordinate
+            Qubit to check neighbours of.
+        displacer : Displacer
+            A displacement to look for another qubit.
+
+        Returns
+        -------
+        QubitCoordinate | None
+            A qubit coordinate or None, if no qubit exists at
+            that displacement.
+        """
+        neighbour = qubit + displacer  # type: ignore
+        try:
+            return next(q for q in self.data_qubits if q == neighbour)
+        except StopIteration:
+            return None
+
+    @abstractmethod
+    def stabilizer_data_qubit_groups(
+        self, stabilizer: QubitCoordinate
+    ) -> list[QubitCoordinate]:
+        """Given a stabilizer qubit, return a list of
+        the data qubits incident on the stabilizer.
+
+        Parameters
+        ----------
+        stabilizer : QubitCoordinate
+            Which stabilizer to consider.
+
+        Returns
+        -------
+        list[QubitCoordinate]
+            List of data qubits incident on the stabilizer.
+        """
+        pass
