ef0215c3eee2da184d46ff59fe0b7888aaac639f
Brendan-Reid1991 <brendan.reid1991@gmail.com>
2024-11-15 14:29:52 +0000

    Renaming decorators->annotations

--- src/dotg/utilities/stim_assets/__init__.py
+++ src/dotg/utilities/stim_assets/__init__.py
@@ -1,6 +1,6 @@
 """Stim assest package. Pythonic objectst that are wrapped around stim commands."""
 
-from dotg.utilities.stim_assets._stim_decorators import StimDecorators
+from dotg.utilities.stim_assets._stim_annotations import StimAnnotations
 from dotg.utilities.stim_assets._stim_gates import (
     MeasureAndReset,
     MeasurementGates,
